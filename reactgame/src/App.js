79c7c8caea53ce7777f3be85668a4b9eddad97e0
Shamil Arefin <landoflitany@gmail.com>
2018-06-21 22:45:09 -0500

    uWu

---
+++ reactgame/src/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import './App.css';
+import Image from './components/Image';
+
+const App = () => 
+
+
+<Image />;
+
+export default App;
---
+++ reactgame/src/components/Image.js
@@ -0,0 +1,38 @@
+import React from "react";
+
+const Image = () => (
+<React.Fragment>
+<div className="row">
+  <div className="col-xs-6 col-md-3">
+    <a href="#" className="thumbnail">
+      <img src="./public/images/amethyst.png" alt="..."/>
+    </a>
+  </div>
+
+
+  <div className="col-xs-6 col-md-3">
+    <a href="#" className="thumbnail">
+      <img src="./public/images/garnet.png" alt="..."/>
+    </a>
+  </div>
+
+
+
+  <div className="col-xs-6 col-md-3">
+    <a href="#" className="thumbnail">
+      <img src="./public/images/peridot.png" alt="..."/>
+    </a>
+  </div>
+
+
+
+  <div className="col-xs-6 col-md-3">
+    <a href="#" className="thumbnail">
+      <img src="./public/images/smokeyquartz.png" alt="..."/>
+    </a>
+  </div>
+</div>
+</React.Fragment>
+);
+
+export default Image;
